a3ea742a6c44b5210be264bac8db2ff3a591bded
lukafaccini <60987084+lukafaccini@users.noreply.github.com>
2020-03-04 18:33:43 -0300

    Add files via upload

---
+++ labelframe/LabelTest.java
@@ -0,0 +1,14 @@
+
+package labelframe;
+
+import javax.swing.JFrame;
+    
+public class LabelTest{
+
+    public static void main (String[] args){
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(260, 180);
+        labelFrame.setVisible(true);
+    }
+}
